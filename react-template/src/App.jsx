2d899a4fa1c7dee1fcfe5fb027d98127395cf11c
tomschmosby <57222062+tomschmosby@users.noreply.github.com>
2022-11-17 19:01:24 +0100

    first

--- react-template/src/App.jsx
+++ react-template/src/App.jsx
@@ -1,32 +1,23 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import './App.css'
+import { Button } from './components/Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const isValid = true;
+
+  const user = {
+    name: "Tom",
+    age: 28, 
+    hobbies: ["Skiing", "Cooking"],
+  }
 
   return (
-    <div className="App">
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src="/vite.svg" className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://reactjs.org" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+    <div>
+      <p>{`The user ${user.name} is ${user.age} years old`} </p>  
+      <Button 
+      text = "My cool Button" 
+      variant="primary"
+      />
     </div>
   )
 }
---
+++ react-template/src/components/Button.jsx
@@ -0,0 +1,9 @@
+import './Button.css'
+
+export function Button(props){
+    return (
+
+        <button className={props.variant === "primary" ? "primary" : "secondary"}> {props.text}</button>
+
+    )
+}
