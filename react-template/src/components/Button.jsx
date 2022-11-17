8a92c44a161ac58a1f5973bac89539b92c5376c5
Andjasrec <64201123+Andjasrec@users.noreply.github.com>
2022-11-17 19:26:28 +0100

    untterricht

---
+++ react-template/src/components/Button.jsx
@@ -0,0 +1,10 @@
+import './Button.css'
+
+export function Button(props) {
+    return(
+        <button 
+        className={props.variant === "primary" ? "primary" : "secondary"}>    
+            {props.text}
+        </button>
+    )
+}
