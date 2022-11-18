85af0b47a8aa55a43e1024df733cf503c4132b27
tomschmosby <57222062+tomschmosby@users.noreply.github.com>
2022-11-18 16:13:38 +0100

    Bis Button imort

--- react-template/src/App.jsx
+++ react-template/src/App.jsx
@@ -1,25 +1,45 @@
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
-import { Button } from './components/Button';
+import {Button} from './components/Button';
+import {ButtonVariants} from './components/Button';
 
 function App() {
-  const isValid = true;
-
-  const user = {
-    name: "Tom",
-    age: 28, 
-    hobbies: ["Skiing", "Cooking"],
-  }
-
   return (
-    <div>
-      <p>{`The user ${user.name} is ${user.age} years old`} </p>  
-      <Button 
-      text = "My cool Button" 
-      variant="primary"
-      />
+
+    <div className = "Modal">
+
+      <div className = "Header">
+          <h3>Select file type</h3>
+          <p>Select the type of file you want to download.</p>  
+          
+
+          <div className = "Body"> 
+            <Button 
+              text=" .zip"
+              variant={ButtonVariants.secondary}
+            />
+            <Button 
+              text=" .tap"
+              variant={ButtonVariants.secondary}
+            /> 
+          </div>
+
+
+          <div className = "Footer"> 
+            <Button
+              text = "Download files"
+              variant = {ButtonVariants.primary}
+            />
+            <Button
+              text = "Cancel"
+              variant = {ButtonVariants.tertiary}
+            />
+        </div>
+
+      </div>
+
     </div>
-  )
-}
+    )
+};
 
 export default App
--- react-template/src/components/Button.jsx
+++ react-template/src/components/Button.jsx
@@ -1,9 +1,20 @@
-import './Button.css'
+import './Button.css';
 
-export function Button(props){
+
+export const ButtonVariants = {
+    primary: 'primary',
+    secondary: 'secondary',
+    tertiary:   'tertiary'
+  }
+
+export const Button = (props) => {
+    const variantSyling = props.variant== ButtonVariants.primary ? ButtonVariants.primary ? ButtonVariants.secondary : ButtonVariants.tertiary ;
+    
     return (
 
-        <button className={props.variant === "primary" ? "primary" : "secondary"}> {props.text}</button>
+        <button className={variantStyling}>{props.text}</button>
 
     )
+
+
 }
